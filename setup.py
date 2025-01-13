e7387e80d38c9743edadccb86f0c5f1466e8865e
Maria Karanasou <karanasou@gmail.com>
2025-01-13 09:13:16 +0200

    Bumping version to 1.2.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = [
 ]
 
 setup(name='pyaml_env',
-      version='1.2.1',
+      version='1.2.2',
       description='Provides yaml file parsing with '
                   'environment variable resolution',
       long_description=long_description,
